2c2a6c27a40748819e645e5dd431b99c22e488d6
arifraj199 <karif9514@gmail.com>
2024-04-23 01:22:08 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,12 +29,24 @@ async function run() {
 
     const userCollection = client.db("dbUser").collection("user");
 
-    app.post('/users',async(req,res)=>{
-        const user = req.body;
-        console.log(user);
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-    })
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
